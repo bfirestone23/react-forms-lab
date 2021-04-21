e36d95293fb4b8c61843157eb27e76748eb6e32f
Brian Firestone <bfirestone2339@gmail.com>
2021-04-21 13:47:39 -0700

    commit tree ebeaf256c38210922d6748482eadd63f6b4387fc

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,34 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+          type="text" 
+          name="message" 
+          id="message" 
+          value={this.state.message}
+          onChange={e => this.handleChange(e)}
+        />
+        <br></br>
+        <span>Characters remaining: {this.props.maxChars - this.state.message.length}</span>
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
